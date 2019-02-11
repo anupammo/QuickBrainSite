0d9227568fef86601dbdfb87963da1c83a1824e9
anupammo <a09051985@gmail.com>
2019-02-12 00:48:39 +0530

    second commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickBrainSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+
+            return View();
+        }
+
+        public ActionResult Execution()
+        {
+
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuickBrainSite.MvcApplication" Language="C#" %>
